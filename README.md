2c46a2f47199d2599bf999359f299bda85c232a8
bennettoh <bennettoh.arch@gmail.com>
2020-10-03 18:04:20 -0700

    implement merge sort

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 Sorting Visualizer (WIP)
 ---
-<img src="./src/logo.svg" width="60px" height="60px" />
-
-Created with *create-react-app*. See the [full create-react-app guide](https://github.com/facebook/create-react-app/blob/master/packages/cra-template/template/README.md).
+<!-- <img src="./src/logo.svg" width="60px" height="60px" /> -->
 
 
-To do
+Features
 ---
 - [x] Live version on gh-pages
 - [x] Basic UI
@@ -17,21 +15,10 @@ To do
 - [ ] Better styling
 
 
-Try It
+Live demo
 ---
 
 [bennettoh.github.io/sorting-visualizer](https://bennettoh.github.io/sorting-visualizer/)
 
 
-
-Install
----
-
-`npm install`
-
-
-
-Usage
----
-
-`npm start`
+Created with *create-react-app*. See the [full create-react-app guide](https://github.com/facebook/create-react-app/blob/master/packages/cra-template/template/README.md).
--- src/App.js
+++ src/App.js
@@ -7,13 +7,126 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    array: [37, 97, 72, 81, 56, 28, 77, 71, 31, 45, 27, 46, 45, 69, 73, 43, 67, 38, 43, 86, 95, 44, 85, 84, 54, 69, 90, 57, 20, 35],
+    array: [],
     algorithm: 'Bubble Sort'
   }
 
-  handleSwap = () => {
+  handleStart = () => {
+    if (this.state.algorithm === 'Bubble Sort') {
+      this.bubbleSort(0, this.state.array.length - 1);
+    }
+    if (this.state.algorithm === 'Merge Sort') {
+      this.setState({
+        array: this.mergeSortRecursive(this.state.array),
+      });
+    }
+    if (this.state.algorithm === 'Quick Sort') {
+      this.quickSort();
+    }
+  }
+
+  recursionTest(counter) {
+    if (counter === 20) return;
+    this.recursionTest(++counter);
+
+    setTimeout(() => {
+      console.log(counter);
+    }, (20 - counter) * 100);
+  }
+
+  bubbleSort(i, length) {
+    setTimeout(() => {
+      if (this.state.array[i % length] > this.state.array[(i % length) + 1]) this.handleSwap(i % length, i % length + 1);
+      if (i < length * length) this.bubbleSort(++i, length);
+    }, 10);
+  }
+
+  mergeSort(array, depth) {
+    if (array.length === 1) return array;
+
+    //pointers
+    let l, m, r;
+    l = 0;
+    m = array.length / 2;
+    r = array.length;
+
+    let arrayA = array.slice(l, m);
+    let arrayB = array.slice(m, r);
+    let newArray;
+
+    arrayA = this.mergeSort(array.slice(0, m), ++depth);
+    arrayB = this.mergeSort(array.slice(m), ++depth);
+
+    setTimeout(() => {
+      console.log('%ccurrent depth: ' + depth, "color: red");
+      console.log('input array ' + arrayA + arrayB);
+
+      newArray = this.merge(arrayA, arrayB);
+
+      this.setState({
+        array: newArray,
+      });
+
+      return newArray;
+    }, (10 - depth) * 100);
+  }
+
+  merge(arrayA, arrayB) {
+    let arrayNew = [];
+    let A = 0;
+    let B = 0;
+
+    while (arrayA.length > 0 && arrayB.length > 0) {
+      if (arrayA[A] < arrayB[B]) {
+        arrayNew.push(arrayA.shift());
+      } else {
+        arrayNew.push(arrayB.shift());
+      }
+    }
+    if (arrayA.length !== 0) arrayNew = arrayNew.concat(arrayA);
+    if (arrayB.length !== 0) arrayNew = arrayNew.concat(arrayB);
+
+    console.log('final array ' + arrayNew);
+
+
+    return arrayNew;
+  }
+
+  mergeSortRecursive(array) {
+    if (array.length === 1) return array;
+
+    let middle = array.length / 2;
+    let arrayA, arrayB, arrayNew = [];
+
+    // pointers
+    let A = 0;
+    let B = 0;
+
+    arrayA = this.mergeSortRecursive(array.slice(0, middle));
+    arrayB = this.mergeSortRecursive(array.slice(middle, array.length));
+
+    while (A < arrayA.length && B < arrayB.length) {
+
+      if (arrayA[A] > arrayB[B]) {
+        arrayNew = arrayNew.concat(arrayB[B++]);
+      } else {
+        arrayNew = arrayNew.concat(arrayA[A++]);
+      }
+    }
+
+    if (A < arrayA.length) arrayNew = arrayNew.concat(arrayA.slice(A));
+    if (B < arrayB.length) arrayNew = arrayNew.concat(arrayB.slice(B));
+
     this.setState({
-      array: swap(this.state.array, 0, 2),
+      array: arrayNew,
+    })
+
+    return arrayNew;
+  }
+
+  handleSwap = (indexA, indexB) => {
+    this.setState({
+      array: swap(this.state.array, indexA, indexB),
     })
   }
 
@@ -26,11 +139,18 @@ class App extends React.Component {
   generateBars = () => {
     let barsTemp = [];
     for (let i = 0; i < 30; i++) {
-      barsTemp.push(Math.floor(Math.random() * 80) + 20)
+      barsTemp.push(Math.floor(Math.random() * 90) + 10)
     }
     this.setState({
       array: barsTemp,
-    })
+    });
+    console.log('=== new bars ===');
+
+    console.log(barsTemp);
+  }
+
+  componentDidMount() {
+    this.generateBars();
   }
 
   render() {
@@ -47,7 +167,7 @@ class App extends React.Component {
         </FormControl>
 
         <Button variant="contained" color="secondary" onClick={() => this.generateBars()}>Reset</Button>
-        <Button variant="contained" color="secondary" onClick={() => this.handleSwap()}>Start</Button>
+        <Button variant="contained" color="secondary" onClick={() => this.handleStart()}>Start</Button>
         <div className="container">
           {barsDiv}
         </div>
